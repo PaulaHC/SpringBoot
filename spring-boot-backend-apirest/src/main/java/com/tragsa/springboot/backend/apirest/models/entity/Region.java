3680595b31291d34108ba567dd635b432a395280
PaulaHCA <paulahca53@gmail.com>
2023-04-21 09:05:45 +0200

    up-backend

---
+++ spring-boot-backend-apirest/src/main/java/com/tragsa/springboot/backend/apirest/models/entity/Region.java
@@ -0,0 +1,34 @@
+package com.tragsa.springboot.backend.apirest.models.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "regiones")
+public class Region {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nombre;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
---
+++ spring-boot-backend-apirest/src/main/java/com/tragsa/springboot/backend/apirest/models/services/IclienteService.java
@@ -0,0 +1,25 @@
+package com.tragsa.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.tragsa.springboot.backend.apirest.models.entity.Cliente;
+import com.tragsa.springboot.backend.apirest.models.entity.Region;
+
+public interface IclienteService {
+
+	public List<Cliente> findAll();
+	
+	public Page<Cliente> findAll(Pageable pageable);
+	
+	public Cliente findById(Long id);
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete(Long id);
+	
+	public List <Region> findAllRegiones();
+
+}
